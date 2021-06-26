c6691b6ebe50006acd7a5a5578ab5406695f07bc
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-06-26 12:44:15 +0900

    Filter: add ForceGuessAlways and support BOM code (always UTF8)

--- filter_windows.go
+++ filter_windows.go
@@ -2,16 +2,30 @@ package mbcs
 
 import (
 	"bufio"
+	"bytes"
 	"io"
 	"unicode/utf8"
+)
+
+type _Status int
+
+const (
+	_GuessMinimum _Status = iota
+	_GuessAlways
+	_AnsiFixed
+	_Utf8Fixed
 )
 
 type Filter struct {
 	sc       *bufio.Scanner
 	text     string
-	ansi     bool
+	status   _Status
 	err      error
 	codepage uintptr
+}
+
+func (f *Filter) ForceGuessAlways() {
+	f.status = _GuessAlways
 }
 
 func NewFilter(r io.Reader, codepage uintptr) *Filter {
@@ -21,20 +35,41 @@ func NewFilter(r io.Reader, codepage uintptr) *Filter {
 	}
 }
 
+var _BOM = []byte{0xEF, 0xBB, 0xBF}
+
 func (f *Filter) Scan() bool {
 	if !f.sc.Scan() {
 		f.err = f.sc.Err()
 		return false
 	}
 	line := f.sc.Bytes()
-	if !f.ansi && utf8.Valid(line) {
+	if f.status == _Utf8Fixed {
 		f.text = f.sc.Text()
-	} else {
-		f.text, f.err = AtoU(line, f.codepage)
-		if f.err != nil {
-			return false
+		return true
+	}
+	if f.status != _AnsiFixed {
+		if bytes.HasPrefix(line, _BOM) {
+			f.text = string(line[len(_BOM):])
+			if f.status != _GuessAlways {
+				f.status = _Utf8Fixed
+			}
+			return true
 		}
-		f.ansi = true
+		if utf8.Valid(line) {
+			f.text = f.sc.Text()
+			// f.status should not be fixed yet,
+			// because it does not work expected
+			// when the first line uses ascii-code only
+			// and the second line uses CP932.
+			return true
+		}
+	}
+	f.text, f.err = AtoU(line, f.codepage)
+	if f.err != nil {
+		return false
+	}
+	if f.status != _GuessAlways {
+		f.status = _AnsiFixed
 	}
 	return true
 }
