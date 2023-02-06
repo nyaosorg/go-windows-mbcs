4a9ca4d05585140fe15a3d20a8736f95639355b5
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-02-07 00:32:34 +0900

    Add tests for AnsiToUtf8 and Utf8ToAnsi

--- main_test.go
+++ main_test.go
@@ -1,7 +1,9 @@
 package mbcs_test
 
 import (
+	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
 	"runtime"
 	"strings"
@@ -31,5 +33,48 @@ func TestConsoleCP(t *testing.T) {
 
 	if fmt.Sprintf("%d", actual) != fields[len(fields)-1] {
 		t.Fatalf("ConsoleCP()==%d, chcp->%s", actual, outputString)
+	}
+}
+
+func readJugemu(t *testing.T) (cp932, utf8 []byte) {
+	var err error
+	cp932, err = os.ReadFile("jugemu-cp932.txt")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	utf8, err = os.ReadFile("jugemu-utf8.txt")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	return cp932, utf8
+}
+
+func TestAnsiToUtf8(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		println("not windows")
+		return
+	}
+	cp932, utf8 := readJugemu(t)
+	actual, err := mbcs.AnsiToUtf8(cp932, 932)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if !bytes.Equal(utf8, []byte(actual)) {
+		t.Fatalf("not equal\n%#v\n%#v", utf8, []byte(actual))
+	}
+}
+
+func TestUtf8ToAnsi(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		println("not windows")
+		return
+	}
+	cp932, utf8 := readJugemu(t)
+	actual, err := mbcs.Utf8ToAnsi(string(utf8), 932)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if !bytes.Equal(cp932, actual) {
+		t.Fatalf("not equal\n%#v\n%#v", cp932, actual)
 	}
 }
