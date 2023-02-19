133c5809630e113cfe900837b9f67b9f5fdef214
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-02-19 09:26:06 +0900

    import const from <windows.h>

---
+++ const.go
@@ -0,0 +1,18 @@
+package mbcs
+
+//go:generate go run github.com/nyaosorg/go-importconst
+//	<windows.h>
+//	CP_ACP
+//	CP_MACCP
+//	CP_OEMCP
+//	CP_SYMBOL
+//	CP_THREAD_ACP
+//	CP_UTF7
+//	CP_UTF8
+//	MB_COMPOSITE
+//	MB_ERR_INVALID_CHARS
+//	MB_PRECOMPOSED
+//	MB_USEGLYPHCHARS
+//	WC_COMPOSITECHECK
+//	WC_ERR_INVALID_CHARS
+//	WC_NO_BEST_FIT_CHARS
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.18
 require golang.org/x/sys v0.5.0
 
 require golang.org/x/text v0.6.0
+
+require github.com/nyaosorg/go-importconst v0.2.0 // indirect
